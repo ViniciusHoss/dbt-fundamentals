e10f8a0a945d6658f781a6cc7c957185f8233423
VHoss <vinichoss@gmail.com>
2022-01-21 14:28:26 +0000

    sources and project organization

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,14 @@
+{{ config( materialized='table' ) }}
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from {{ source('jaffle_shop', 'customers') }}
+
+)
+
+select * from customers
